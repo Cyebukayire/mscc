00223cab265319f7e7103473e707155cb0deb73c
Cyebukayire <peacecyebukayire@gmail.com>
2023-12-27 18:50:02 -0500

    feat update routes and services

--- routers/comments.py
+++ routers/comments.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from services.comments import fetch_comment
-from utils.word_counter import word_counter
-from utils.temp_store import store_metadata
+from controllers.comments import get_comment_metadata
 
 router = APIRouter(
     prefix = "/comments",          
@@ -11,51 +9,6 @@ router = APIRouter(
 
 # Retrieve metadata of a single comment
 @router.get("/{comment_id}") 
-async def read_comment(comment_id: str):
-    response = await fetch_comment(comment_id)
-    response_data = response.json()
-
-    # Extra metadata
-    if response.status_code == 200:
-        comment_data = response_data['data']['attributes']
-        
-        # Check if the comment has an attachement and extract all simple metadata (EX: comment_id = 'COLC-2023-0006-0036')
-        if 'included'in response_data:            
-            file_url = []
-            file_name = []
-            file_size = 0
-            for attachment in response_data['included']:
-                file_url.append(attachment['attributes']['fileFormats'][0]['fileUrl'])
-                file_name.append(attachment['attributes']['title'])
-                file_size += attachment['attributes']['fileFormats'][0]['size']
-
-            word_count = word_counter(file_url)
-
-            simple_metadata = {
-                "comment_id": comment_id,
-                "comment_title": comment_data['title'], 
-                "file_url": file_url,
-                "file_name": file_name, 
-                "file_size": file_size,
-                "word_count": word_count
-            }
-            # Store metadata
-            store_metadata(simple_metadata)
-
-            return simple_metadata
-        
-        # If comment has no attachement, extract few metadata (Ex: comment_id = 'COLC-2023-0006-0862')
-        else:
-            word_count = word_counter(comment_data['comment'])
-            simple_metadata = {
-                "comment_id": comment_id,
-                "comment_title": comment_data['title'], 
-                "file_url": "No attached document",
-                "file_name": "Does not apply", 
-                "file_size": "Does not apply",
-                "word_count": word_count
-            }
-            store_metadata(simple_metadata)
-            return simple_metadata
-    else:
-        raise HTTPException(status = response.status_code, message = "failed to fetch comment")
+async def get_comment(comment_id: str):
+    simple_metadata = await get_comment_metadata(comment_id)
+    return simple_metadata
--- services/comments.py
+++ services/comments.py
@@ -1,10 +1,11 @@
 import httpx
 from config.config import settings
 
-# Fetch a comment from Regulations.gov API 
-async def fetch_comment(comment_id):
-    api_key = settings.API_KEY
+# Get a comment from Regulations.gov API 
+async def get_comment(comment_id):
+    regulations_api_key = settings.REGULATIONS_API_KEY
     async with httpx.AsyncClient() as client:
-        comment_api = f"https://api.regulations.gov/v4/documents/{comment_id}?include=attachments&api_key={api_key}"
+        comment_api = f"https://api.regulations.gov/v4/documents/{comment_id}?include=attachments&api_key={regulations_api_key}"
         response = await client.get(comment_api)
         return response
+    
