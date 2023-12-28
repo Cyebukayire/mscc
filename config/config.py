ff1d6923475a2ea4208ffecde53567ebd8ffc18b
Cyebukayire <peacecyebukayire@gmail.com>
2023-12-28 07:20:18 -0500

    feat configure environment

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 # Setting up secret environment variables
 class Settings(BaseSettings):
     APP_NAME: str = "Making Sense Of The Copyright Office Comments"
-    API_KEY: str
+    REGULATIONS_API_KEY: str
+    OPENAI_API_KEY: str
 
 settings = Settings()
