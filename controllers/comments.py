894e31235b1a61176f153f1db75556531479e674
Cyebukayire <peacecyebukayire@gmail.com>
2024-02-04 21:58:12 -0500

    Update author extraction

--- controllers/comments.py
+++ controllers/comments.py
@@ -1,9 +1,15 @@
 from fastapi import HTTPException
 from services import comments as comment_service
-from utils import counter, data_extractor, temp_store
-from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
+from utils import counter, data_extractor, temp_store, case_law_citation_patterns
 from config.config import settings
-import json
+# import json
+# import stanza
+# import torch
+from models import bart_large, distillbert_cased
+from eyecite import get_citations, clean_text, resolve_citations, annotate_citations
+from eyecite.tokenizers import HyperscanTokenizer
+
+import re
 
 # Retrieve metadata of a single comment
 async def get_comment_metadata(comment_id: str):
@@ -28,8 +34,15 @@ async def get_comment_metadata(comment_id: str):
                 document_size += attachment['attributes']['fileFormats'][0]['size']
 
             comment_content = data_extractor.text_extractor(document_url)
-            word_count = counter.word_counter(comment_content)
+            # word_count = counter.word_counter(comment_content)
+            word_count = 0
+            # word_count = 0
             authors = get_comment_authors(comment_title, comment_content)
+            # authors = ""
+            # tech_tools = get_tech_tools(comment_content)
+            tech_tools = []
+            # cited_case_laws = get_cited_case_laws(comment_content)
+            cited_case_laws = []
             simple_metadata = {
                 "comment_id": comment_id,
                 "comment_title": comment_title,
@@ -37,26 +50,27 @@ async def get_comment_metadata(comment_id: str):
                 "document_name": document_name, 
                 "document_size": document_size,
                 "word_count": word_count,
-                "authors": authors
+                "authors": authors,
+                "tech_tools": tech_tools,
+                "cited_case_laws": cited_case_laws
             }
 
             # Store metadata
             temp_store.store_metadata(simple_metadata)
 
-
-            data = {
-                "comment_id": comment_id,
-                "comment_title": comment_title,
-                "document_url": document_url,
-                "document_name": document_name, 
-                "document_title":"",
-                "document_size": document_size,
-                "word_count": word_count,
-                "authors": authors,
-                "document_content": comment_content
-            }
-            with open('/Users/peace/Developer/Machine Learning/Research/mscc/utils/text.json', 'w') as file:
-                json.dump(data, file, indent=4)
+            # data = {
+            #     "comment_id": comment_id,
+            #     "comment_title": comment_title,
+            #     "document_url": document_url,
+            #     "document_name": document_name, 
+            #     "document_title":"",
+            #     "document_size": document_size,
+            #     "word_count": word_count,
+            #     "authors": authors,
+            #     "document_content": comment_content
+            # }
+            # with open('/Users/peace/Developer/Machine Learning/Research/mscc/utils/text.json', 'w') as file:
+            #     json.dump(data, file, indent=4)
 
             return simple_metadata
         
@@ -64,7 +78,13 @@ async def get_comment_metadata(comment_id: str):
         else:
             comment_content = comment_data['comment']
             authors = get_comment_authors(comment_title, comment_content)
-            word_count = counter.word_counter(comment_content)
+            # authors=""
+            # word_count = counter.word_counter(comment_content)
+            word_count = 0
+            # tech_tools = get_tech_tools(comment_content)
+            tech_tools=[]
+            # cited_case_laws= get_cited_case_laws(comment_content)
+            cited_case_laws = []
             simple_metadata = {
                 "comment_id": comment_id,
                 "comment_title": comment_title, 
@@ -72,8 +92,11 @@ async def get_comment_metadata(comment_id: str):
                 "file_name": "Does not apply", 
                 "file_size": "Does not apply",
                 "word_count": word_count,
-                "authors": authors
+                "authors": authors,
+                "tech_tools": tech_tools,
+                "cited_case_laws": cited_case_laws
             }
+
             # store metadata
             temp_store.store_metadata(simple_metadata)
 
@@ -81,53 +104,34 @@ async def get_comment_metadata(comment_id: str):
         
     else:
         raise HTTPException(status = response.status_code, message = "failed to fetch comment")
-
-
+    
 
 def get_comment_authors(comment_title, comment_content: str):
-    # create smaller chunks of text from the comment
-    #segments = [comment_content[i : i + segment_size] for i in range(0, len(comment_content), segment_size)]
+    question = "Who wrote this comment?"
+    comment = comment_title + ' ' + comment_content.replace('\n', ' ')
+    comment_size = len(comment)
 
-    # initialize nlp pipleline
-    '''
-    When it comes to extracting citations from text, Named Entity Recognition (NER) models can be quite effective. Here are several models from Hugging Face's model hub that you might consider trying for author name extraction:
-    0. GPT 3.5
-    1. BERT-based Models: dslim/bert-base-NER: 
-    2. RoBERTa-based Models: allenai/longformer-base-4096: 
-    3. DistilBERT-based Models: distilbert-base-uncased
-    4. ALBERT-based Models: albert-large-v2
-    '''
-    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    # Limit large document input
+    first_chunk ="" # The first 1024 tokens of the document
+    last_chunk="" # The last tokens of the document
+    if comment_size > 1024:
+        first_chunk = comment[:1024]
+        last_chunk = comment[(comment_size-1024):]
 
-    nlp_pipeline = pipeline("ner", model="dslim/bert-base-NER", grouped_entities=True)
-    # nlp_pipeline = pipeline("ner", model=model, tokenizer=tokenizer, grouped_entities=True)
-    # send an author extraction request to huggingface transformers
-    prompt = f"What are the authors of this document below? \n Document title: {comment_title} \n \"{comment_content}\"."
-    # prompt = f"Identify full names of authors of this comment: \"{comment_content}\""
-    response = nlp_pipeline(prompt)
-    print(response)
-    
-    # extract authors 
-    result = [entity['word'] for entity in response if entity['entity_group'] == 'PER']
-    print(result)
-    authors = list(set(result))
+        # Extract authors with BART-LARGE
+        authors = bart_large.question_answering(question=question, text=first_chunk)
 
+        # Extract from last chunk if the author was not found in first chunk
+        if (len(authors) == 0):
+            authors=(bart_large.question_answering(question=question, text=last_chunk))
+
+        print("\n\n\n\n\n AUTHORS:\n", authors, "\n\n\n\n")
+        # authors = list(set(authors))
+        print("\n\n\nFIRST CHUNK\n", first_chunk, "\n\n\n",last_chunk, " \n\n\n\n")
+
+    else:
+        # Extract authors with BART-LARGE
+        authors = bart_large.question_answering(question=question, text=comment)
+
+        
     return authors
-
-    # import spacy
-
-    # # Load the English language model
-    # nlp = spacy.load("en_core_web_sm")
-
-    # # Your text containing author names
-    # text = "title: " + comment_title + "\n" + comment_content
-    # # text = comment_content 
-    # # Process the text with spaCy
-    # doc = nlp(text)
-
-    # # Extract entities that are people (PERSON entity type)
-    # newauthors = [entity.text for entity in doc.ents if entity.label_ == "PERSON"]
-
-    # print("Authors found:", newauthors)
-    # return authors
