3d8afc1f5e5394eacac04c3aec92146d3b38c1aa
Cyebukayire <peacecyebukayire@gmail.com>
2024-02-11 21:43:00 -0500

    add bart-large model

---
+++ models/bart_large.py
@@ -0,0 +1,28 @@
+from transformers import BartForQuestionAnswering, BartTokenizer
+import torch
+
+
+# BART Model using pytorch
+def question_answering(question, text):
+    tokenizer = BartTokenizer.from_pretrained('valhalla/bart-large-finetuned-squadv1')
+    model = BartForQuestionAnswering.from_pretrained('valhalla/bart-large-finetuned-squadv1')
+    
+    # Limit large document input
+    if len(text) > 1024:
+        raise ValueError("Error: Large Text Input to BART")
+
+    answers = []
+
+    encoding = tokenizer(question, text, return_tensors='pt')
+    input_ids = encoding['input_ids']
+    attention_mask = encoding['attention_mask']
+
+    start_scores, end_scores = model(input_ids, attention_mask=attention_mask, output_attentions=False)[:2]
+
+    all_tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+    answer = ' '.join(all_tokens[torch.argmax(start_scores): torch.argmax(end_scores) + 1])
+    answer = tokenizer.convert_tokens_to_ids(answer.split())
+    answer = tokenizer.decode(answer)
+    answers.append(answer)
+
+    return answers
