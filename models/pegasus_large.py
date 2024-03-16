b6a2183f6972c4a853d09101ceb855f092461004
Cyebukayire <peacecyebukayire@gmail.com>
2024-03-16 19:30:26 -0400

    chore: test pegasus-large

---
+++ models/pegasus_large.py
@@ -0,0 +1,12 @@
+from transformers import pipeline, PegasusTokenizer, PegasusForConditionalGeneration
+
+# Ver large
+model_name = "google/pegasus-large"
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
+model = PegasusForConditionalGeneration.from_pretrained(model_name)
+
+def summarizing(comment):
+    inputs = tokenizer([comment], max_length=1024, return_tensors='pt', truncation=True)
+    summary_ids = model.generate(inputs['input_ids'], max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
