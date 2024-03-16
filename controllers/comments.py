3a6a3af5cc4cdbd6fbe5ab862c9e8f987744394c
Cyebukayire <peacecyebukayire@gmail.com>
2024-03-15 21:05:59 -0400

    chore: test summarization with t5

--- controllers/comments.py
+++ controllers/comments.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from services import comments as comment_service
 from utils import counter, data_extractor, temp_store
-from models import bart_large 
+from models import bart_large, distillbert_cased, facebook_bert_large, t5
 from eyecite import get_citations, clean_text
 
 # Retrieve metadata of a single comment
@@ -27,20 +27,22 @@ async def get_comment_metadata(comment_id: str):
                 document_size += attachment['attributes']['fileFormats'][0]['size']
 
             comment_content = data_extractor.text_extractor(document_url)
-            word_count = counter.word_counter(comment_content)
-            authors = get_comment_authors(comment_title, comment_content)
-            tech_tools = get_tech_tools(comment_content)
-            cited_case_laws = get_cited_case_laws(comment_content)
+            # word_count = counter.word_counter(comment_content)
+            # authors = get_comment_authors(comment_title, comment_content)
+            # tech_tools = get_tech_tools(comment_content)
+            # cited_case_laws = get_cited_case_laws(comment_content)
+            summary = get_summary(comment_content)
             simple_metadata = {
                 "comment_id": comment_id,
                 "comment_title": comment_title,
                 "document_url": document_url,
                 "document_name": document_name, 
                 "document_size": document_size,
-                "word_count": word_count,
-                "authors": authors,
-                "tech_tools": tech_tools,
-                "cited_case_laws": cited_case_laws
+                # "word_count": word_count,
+                # "authors": authors,
+                # "tech_tools": tech_tools,
+                # "cited_case_laws": cited_case_laws,
+                "summary": summary
             }
 
             # Store metadata
@@ -51,20 +53,23 @@ async def get_comment_metadata(comment_id: str):
         # If comment has no attachement, extract few metadata (Ex: comment_id = 'COLC-2023-0006-0862')
         else:
             comment_content = comment_data['comment']
-            authors = get_comment_authors(comment_title, comment_content)
-            word_count = counter.word_counter(comment_content)
-            tech_tools = get_tech_tools(comment_content)
-            cited_case_laws= get_cited_case_laws(comment_content)
+            # authors = get_comment_authors(comment_title, comment_content)
+            # word_count = counter.word_counter(comment_content)
+            # tech_tools = get_tech_tools(comment_content)
+            # cited_case_laws= get_cited_case_laws(comment_content)
+            summary = get_summary(comment_content)
+            
             simple_metadata = {
                 "comment_id": comment_id,
                 "comment_title": comment_title, 
                 "file_url": "No attached document",
                 "file_name": "Does not apply", 
                 "file_size": "Does not apply",
-                "word_count": word_count,
-                "authors": authors,
-                "tech_tools": tech_tools,
-                "cited_case_laws": cited_case_laws
+                # "word_count": word_count,
+                # "authors": authors,
+                # "tech_tools": tech_tools,
+                # "cited_case_laws": cited_case_laws,
+                "summary": summary,
             }
 
             # store metadata
@@ -77,7 +82,7 @@ async def get_comment_metadata(comment_id: str):
     
 
 def get_comment_authors(comment_title, comment_content: str):
-    question = "Who wrote this comment?"
+    prompt = "Who wrote this comment?"
     comment = comment_title + ' ' + comment_content.replace('\n', ' ')
     comment_size = len(comment)
 
@@ -89,27 +94,27 @@ def get_comment_authors(comment_title, comment_content: str):
         last_chunk = comment[(comment_size-1024):]
 
         # Extract authors with BART-LARGE
-        authors = bart_large.question_answering(question=question, text=first_chunk)
+        authors = bart_large.question_answering(question=prompt, comment=first_chunk)
 
         # Extract from last chunk if the author was not found in first chunk
         if (len(authors) == 0):
-            authors=(bart_large.question_answering(question=question, text=last_chunk))
+            authors=(bart_large.question_answering(question=prompt, comment=last_chunk))
 
     else:
         # Extract authors with BART-LARGE
-        authors = bart_large.question_answering(question=question, text=comment)
+        authors = bart_large.question_answering(question=prompt, comment=comment)
 
     return authors
    
 
 def get_tech_tools(comment_content: str):
-    question = "what are the technologies discussed? Extract only their names." # If not, return none
+    prompt = "what are the technologies discussed? Extract only their names." # If not, return none
     tech_tools = []
 
     # Create smaller text chunks
     chunks = data_extractor.create_text_chunks(comment_content)
     for chunk in chunks:
-        tools = bart_large.question_answering(question=question, text=chunk)
+        tools = bart_large.question_answering(question=prompt, comment=chunk)
     
         # TODO Use NER model to accurately extract only names
         for tool in tools:
@@ -117,10 +122,17 @@ def get_tech_tools(comment_content: str):
                 tools.remove(tool)
 
         # Append list of tools to tech_tools
-        tech_tools.extend(tools)
+        tech_tools.append(tools)
                 
     if len(tech_tools) > 1:
         tech_tools = list(set(tuple(tool) for tool in tech_tools))
+
+        # Remove empty strings from tools
+        while ' 'in tools:
+            tools.remove(' ')
+
+        while '' in tools:
+            tools.remove('')
 
     return tech_tools
 
@@ -139,3 +151,11 @@ def get_cited_case_laws(comment_content: str):
     citations = list(set(citations))
 
     return citations
+
+
+def get_summary(comment_content: str):
+    # Summarize the input comment
+    print("\n\nSUMMARIZING...\n\n")
+    summary = t5.summarizing(comment_content)
+
+    return summary
---
+++ models/t5.py
@@ -0,0 +1,27 @@
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+model_name = 'utrobinmv/t5_summary_en_ru_zh_base_2048'
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+
+def summarizing(comment):        
+    # text summary generate
+    prefix = 'summary: '
+    src_text = prefix + comment
+    input_ids = tokenizer(src_text, return_tensors="pt")
+
+    generated_tokens = model.generate(**input_ids)
+
+    summary = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    return summary
+
+def summarizing_briefly(comment):
+    # text brief summary generate
+    prefix = 'summary brief: '
+    src_text = prefix + comment
+    input_ids = tokenizer(src_text, return_tensors="pt")
+
+    generated_tokens = model.generate(**input_ids)
+
+    summary = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    return summary
