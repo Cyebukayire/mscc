72b2d0c9154142b19cb90180bac289bbd930fd2e
Cyebukayire <peacecyebukayire@gmail.com>
2024-05-27 21:20:09 -0400

    update settings configurations

--- config/config.py
+++ config/config.py
@@ -5,5 +5,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "Making Sense Of The Copyright Office Comments"
     REGULATIONS_API_KEY: str
     OPENAI_API_KEY: str
+    LOCAL_DB_PATH: str
+    CONFIG_PATH: str
 
 settings = Settings()
