4b13ba1be99a625f04009d4e131d42649f453c05
Cyebukayire <peacecyebukayire@gmail.com>
2024-05-31 18:02:40 -0400

    add route

--- routers/comments.py
+++ routers/comments.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter, HTTPException
 from controllers.comments_external_api_controller import get_comment_metadata
+from controllers.comments_local_db_controller import get_all_metadata
 
 router = APIRouter(
     prefix = "/comments",          
     tags = ["comments"],
     responses = {404: {"description": "Not found"}}
 )
+
+# Retrieve metadata of all comments
+@router.get("/all") 
+async def get_comment():
+    all_metadata = await get_all_metadata()
+    return all_metadata
 
 # Retrieve metadata of a single comment
 @router.get("/{comment_id}") 
