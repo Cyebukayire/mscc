dafaa627bfe29b4cf81889d6f44ae4c60164a370
Cyebukayire <peacecyebukayire@gmail.com>
2024-06-01 21:10:18 -0400

    update services

--- services/comments.py
+++ services/comments.py
@@ -1,8 +1,13 @@
 import httpx
 from config.config import settings
 import json
+from collections import namedtuple
 
-database_path = '../database/comments/database.json'
+# namedtuple to format request output
+response_data = namedtuple('response', ['status', 'data', 'message'])
+
+database_path = f"{settings.LOCAL_DB_PATH}database.json"
+
 
 # Get a comment from Regulations.gov API 
 async def get_comment_with_url(comment_id):
@@ -13,19 +18,47 @@ async def get_comment_with_url(comment_id):
         return response
     
 # Returns the list of all comment objects
-async def get_all_comments():
+def get_all_comments():
     # Fetch comment objects from database
-    with open(database_path, 'r') as db:
-        comments = json.load(db)
-    return comments
+    try:
+        with open(database_path, 'r') as db:
+            comments = json.load(db)
+        
+        # Check if the database is empty
+        if len(comments) == 0:
+            return response_data(status=204, data=None, message="No comment found.")
+        
+        # Return all retrieved comments
+        else:
+            return response_data(status=200, data=comments, message="Successful request.")
+    
+    # Handle error exceptions
+    except FileNotFoundError:
+        return response_data(status=500, data=None, message="Server Error: Database not found.")
+    
+    except json.JSONDecodeError:
+        return response_data(status=500, data=None, message="Server Error: Failed to decode database file.")
+    
+    except PermissionError:
+        return response_data(status=500, data=None, message="Server Error: Denied permission to access the database.")
+    
+    except Exception as e:
+        return response_data(status=500, data=None, message="Unexpected server error occured: {e}")
+        
 
 # Returns an object of the comment
-async def get_comment(comment_id):
-    comments = get_all_comments()
-    for comment in comments:
-        if comment['data']['id'] == comment_id:
-            return {"status": 404, "data": comment}
-    
-    # Comment not found
-    return {"status": 404, "data": None}
+def get_comment(comment_id):
+    response = get_all_comments()
 
+    if response.status == 200:
+        comments = response.data
+
+        for comment in comments:
+            if comment['data']['id'] == comment_id:
+                return response_data(status=200, data=comment, message="Successful request.")
+        
+        # Comment not found
+        return response_data(status=404, data=None, message=f"Comment {comment_id} not found.")
+    
+    else:
+        raise Exception(response.status, response.message)
