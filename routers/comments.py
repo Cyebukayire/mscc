49e240f9a9ed5a7bfa841996013cd24a8c686304
Cyebukayire <peacecyebukayire@gmail.com>
2024-06-24 21:42:00 -0400

    chore: remove unused methods

--- routers/comments.py
+++ routers/comments.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from controllers.comments_external_api_controller import get_comment_metadata
-from controllers.comments_local_db_controller import get_all_metadata, update_comment_entity
+from controllers.comments_local_db_controller import get_all_metadata, update_comment_entity, get_cited_case_laws, count_cited_case_laws
 
 router = APIRouter(
     prefix = "/comments",          
@@ -13,6 +13,17 @@ router = APIRouter(
 async def get_comment():
     all_metadata = await get_all_metadata()
     return all_metadata
+
+# Get cited case laws
+@router.get("/citations/all")
+def get_citations():
+    citations = get_cited_case_laws()
+    return citations
+
+@router.get("/citations/rank")
+def get_citations():
+    analysis = count_cited_case_laws()
+    return analysis
 
 # Retrieve metadata of a single comment
 @router.get("/{comment_id}") 
--- utils/word_counter.py
+++ utils/word_counter.py
@@ -1,18 +1,18 @@
 import os
 import requests
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+# from googleapiclient.discovery import build
+# from googleapiclient.http import MediaFileUpload
 from docx import Document
 import pandas as pd
-from config.config import settings
+# from config.config import settings
 
 # Paths
-CREDENTIALS_FILE = f'{settings.CONFIG_PATH}credentials.json'
-TOKEN_FILE = 'token.json'  # Path to save the access token
-SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/documents.readonly']
+# CREDENTIALS_FILE = f'{settings.CONFIG_PATH}credentials.json'
+# TOKEN_FILE = 'token.json'  # Path to save the access token
+# SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/documents.readonly']
 
 
 # Download word document from url
@@ -29,39 +29,39 @@ def download_file(url, local_filename):
                 f.write(chunk)
     return local_filename
 
-def authenticate():
-    creds = None
-    if os.path.exists(TOKEN_FILE):
-        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open(TOKEN_FILE, 'w') as token:
-            token.write(creds.to_json())
-    return creds
+# def authenticate():
+#     creds = None
+#     if os.path.exists(TOKEN_FILE):
+#         creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         with open(TOKEN_FILE, 'w') as token:
+#             token.write(creds.to_json())
+#     return creds
 
-creds = authenticate()
-drive_service = build('drive', 'v3', credentials=creds)
-docs_service = build('docs', 'v1', credentials=creds)
+# creds = authenticate()
+# drive_service = build('drive', 'v3', credentials=creds)
+# docs_service = build('docs', 'v1', credentials=creds)
 
-def upload_to_drive(filepath):
-    file_metadata = {'name': os.path.basename(filepath), 'mimeType': 'application/vnd.google-apps.document'}
-    media = MediaFileUpload(filepath, mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    return file.get('id')
+# def upload_to_drive(filepath):
+#     file_metadata = {'name': os.path.basename(filepath), 'mimeType': 'application/vnd.google-apps.document'}
+#     media = MediaFileUpload(filepath, mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+#     file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+#     return file.get('id')
 
-def count_words_in_doc(doc_id):
-    doc = docs_service.documents().get(documentId=doc_id).execute()
-    text = ''
-    for element in doc.get('body').get('content'):
-        if 'paragraph' in element:
-            for paragraph_element in element.get('paragraph').get('elements'):
-                if 'textRun' in paragraph_element:
-                    text += paragraph_element.get('textRun').get('content')
-    return len(text.split())
+# def count_words_in_doc(doc_id):
+#     doc = docs_service.documents().get(documentId=doc_id).execute()
+#     text = ''
+#     for element in doc.get('body').get('content'):
+#         if 'paragraph' in element:
+#             for paragraph_element in element.get('paragraph').get('elements'):
+#                 if 'textRun' in paragraph_element:
+#                     text += paragraph_element.get('textRun').get('content')
+#     return len(text.split())
 
 def count_words_with_docx_lib(temp_docx_file_path):
     doc = Document(temp_docx_file_path)
